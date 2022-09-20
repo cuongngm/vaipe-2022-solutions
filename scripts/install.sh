1eaabc3ea1962f3c2d8131f19193c722118d7586
cuongnm1 <nguyenmanhcuong02@tech.admicro.vn>
2022-09-20 15:54:50 +0700

    fix prepare

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,17 +2,16 @@ pip install torch==1.10.0+cu111 torchvision==0.11.0+cu111 torchaudio==0.10.0 -f 
 pip install -r requirements.txt
 mkdir checkpoints
 cd checkpoints
-# detect pill model
-gdown --id 11a9N-7AOG5QcswCtvqj1CVdgHWRjzr5i
 # detect drugname model
 gdown --id 1kXMmG6aqRyFAAo2Kr_GmpPL696s6r1i0
 # recog drugname model
 gdown --id 1itEm13CQQ8szInHgPKsVpC860sonTCO6
 # recog pill model
+# dict mapping don thuoc
+gdown --id 1a5gTKsBqrTmg17x2Diz5mo-AoYXGkXwm
+# detect pill model
+gdown --id 1yy7LMOOhot2EQ1Bq4S1XbPVyQNnUFE33
 # extract feature pill model
-gdown --id 1dclf-hsblbzUwXzMAW-N76GxkgNMzTVN
-# download train embeddings
-gdown --id 1fZGlfPSDGjumynedhh2Ehl_c0lweESuu
-# download train targets
-gdown --id 1kFWn0F9mWRe80zUB_-UUmJsKLDk40rG9
+gdown --id 15hvwXB-Vipllgieg-7aqUfPWNTdR8FDZ
+gdown --id 1_j0HfU14JuD45A2h1c1uhMtT8gA2WuPF
 cd ../
